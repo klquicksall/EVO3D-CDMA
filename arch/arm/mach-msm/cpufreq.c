f39f20b25433aece86fd5fa3eea68c7bfcfb7114
Ken <klquicksall@gmail.com>
2012-06-07 10:25:52 -0500

    set boot clock speeds

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -225,6 +225,9 @@ static int __cpuinit msm_cpufreq_init(struct cpufreq_policy *policy)
 	INIT_WORK(&cpu_work->work, set_cpu_work);
 #endif
 
+      /* set safe default min and max speeds */
+        policy->max = 1512000;
+        policy->min = 192000;
 	return 0;
 }
 
